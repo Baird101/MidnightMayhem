725226777ca1e8d334b1980862d9f85155e85c00
Baird101 <bairdilyon@gmail.com>
2025-05-22 11:23:13 -0700

    initial commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,3 +1,7 @@
+// Make sure to include p5.js in your HTML file for this code to work.
+// For example, add the following line in your HTML <head> section:
+// <script src="https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.min.js"></script>
+
 function setup() {
   createCanvas(400, 400);
 }
